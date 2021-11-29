b63d0bec67db301041fab0a551a9d6223dfe08e4
unknown <bapi98deba@gmail.com>
2021-11-29 21:09:13 +0530

    Recursion Easy Problems

---
+++ Factorial of N/Main.java
@@ -0,0 +1,16 @@
+// Factorial of N; Recursion Easy Problem
+// date: 29.11.2021
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Factorial(n));
+    }
+
+    public static int Factorial(int n) {
+        if(n == 1) return 1;
+        return Factorial(n-1) * n;
+    }
+}
